a912a3983bb56ba3cad73e47939c529c9af62b3e
AbhiNav8084 <shubhamkumarsingh415@gmail.com>
2026-05-11 22:14:42 +0530

    Custom Hooks (useAuth),Two-Way Binding, CORS Setup, Protected Routes & getMe Integration

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,6 +1,7 @@
 import {createBrowserRouter} from "react-router";
 import Register from "./features/auth/pages/Register";
 import Login from "./features/auth/pages/Login";
+import Protected from "./features/auth/components/Protected";
 
 
 
@@ -14,5 +15,9 @@ export const router = createBrowserRouter([
     {
         path: "/register",
         element: < Register />
+    },
+    {
+        path: "/",
+        element: <Protected> <h1>Home Page</h1> </Protected>
     }
 ])
